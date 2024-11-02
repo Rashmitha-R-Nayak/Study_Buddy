b85fb907801c90a994348d655c3c746cc28320de
Dharshansk16 <dcodecraft@gmail.com>
2024-11-02 20:21:07 +0530

    feat:Dropzone Hero animation

---
+++ frontend/src/app/(root)/_components/hero/Dropzone.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+export default function DropZone() {
+  return (
+    <form className="flex items-center justify-center w-[900px] h-[225px] sm:p-4">
+      <Label
+        htmlFor="dropzone-file"
+        className="flex flex-col items-center justify-center w-full h-full transition duration-500 ease-in-out hover:scale-105   rounded-2xl cursor-pointer  bg-gray-800"
+      >
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+          <svg
+            className="w-8 h-8 mb-4 text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          <p className="mb-2 text-xl text-gray-400">
+            <span className="font-semibold">Click to upload</span> or drag and
+            drop
+          </p>
+          <p className="text-lg text-gray-400">.pdf</p>
+        </div>
+        <Input id="dropzone-file" type="file" name="file" className="hidden" />
+      </Label>
+    </form>
+  );
+}
--- frontend/src/app/(root)/_components/hero/Hero.tsx
+++ frontend/src/app/(root)/_components/hero/Hero.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import Heading from "../common/Heading";
 import SubHeading from "../common/SubHeading";
+import DropZone from "./Dropzone";
+import styles from "./Hero.module.css";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col gap-1 md:gap-4 items-center text-center">
+    <div
+      className={`${styles.heroBackground} flex flex-col gap-1 md:gap-4 items-center text-center`}
+    >
+      <div className={styles.wavePattern}></div>
+
+      <div className={styles.geometricShape}></div>
+
       <Heading
-        className="font-extrabold  leading-6 p-2 md:p-4"
+        className="font-extrabold  leading-6 p-2 md:p-6"
         lineOneText="Revolutionize Your Study"
         lineTwoText="Routine With Study Buddy"
       />
       <SubHeading
-        className="scroll-m-20 mx-auto  font-[500] max-w-3xl text-center text-[16px] sm:text-2xl text-zinc-400"
+        className="scroll-m-20 mx-auto leading-10 font-[500] max-w-3xl text-center text-[16px] sm:text-2xl text-zinc-500"
         lineOneText="Effortlessly interact with your PDFs, notes, resumes, and more."
         lineTwoText="Enhancing your learning experience and productivity like never before"
       />
+      <DropZone />
     </div>
   );
 }
